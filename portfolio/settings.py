b0b5239439245741e3fab614cdbc3f1f4a7f2dcf
logiflo <logiflo5@gmail.com>
2020-09-09 12:45:01 +0100

    Modify ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -56,7 +56,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     #Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['https://lorenagil-portfolio.herokuapp.com']
 
     #Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
